5388c723dedd134c2ba066f9a7e0998f4bc0605d
Virk0 <virko.veelmaa@tptlive.ee>
2017-11-07 09:41:27 +0200

    no save

--- Grupitoo/Program.cs
+++ Grupitoo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,54 @@ namespace Grupitoo
         }
 
 
-        static void fileOpening(string[] args)
+        //static void fileOpening(string[] args)
+        //{
+        //    using (StreamReader sr = new StreamReader("Note1.txt")) ;
+        //    string line;
+        //    while ((line = sr.ReadLine()) != null)
+        //    {
+        //        Console.WriteLine(line);
+
+        //    }
+
+
+
+        //}
+        static void WritingInFiles(string[] args)
         {
-            StreamReader sr = new StreamReader("Note1.txt") ;
-            string line;
-            while ((line = sr.ReadLine()) != null)
+            // Create a string array with the lines of text
+            string[] lines = { "First line", "Second line", "Third line" };
+
+            // Set a variable to the My Documents path.
+            string path = @"C:\Users\opilane\Documents\GitHub\Grupitoo\Grupitoo\Notes";
+
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(path + @"\WriteLines.txt"))
             {
-                Console.WriteLine(line);
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
             }
         }
+        static void ReadingInFiles(string[] args)
+        {
+                StreamReader objReader = new StreamReader("c:\\");
+                string sLine = "";
+                ArrayList arrText = new ArrayList();
+
+                while (sLine != null)
+                {
+                    sLine = objReader.ReadLine();
+                    if (sLine != null)
+                        arrText.Add(sLine);
+                }
+                objReader.Close();
+
+                foreach (string sOutput in arrText)
+                    Console.WriteLine(sOutput);
+                Console.ReadLine();
+    
+        }
+        
+
     }
 }
