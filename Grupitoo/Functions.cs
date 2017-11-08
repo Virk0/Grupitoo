fdce947f228e0f1a732d6c793637d919f609f93a
Virk0 <33309339+Virk0@users.noreply.github.com>
2017-11-08 21:49:06 +0200

    Testing adin

---
+++ Grupitoo/Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Grupitoo
+{
+    class NoteFunctions
+    {
+        static string Path = "Notes.txt";
+        public static void CreateText()
+        {
+            File.Create(Path);
+
+        }
+
+        
+    }
+}
